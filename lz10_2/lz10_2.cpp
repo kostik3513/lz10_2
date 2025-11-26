3828bc705f3054a7eaa5705e7818bc0921b2924c
kostik3513 <kostik3513@gmail.com>
2025-11-26 15:35:53 +0200

    Add project files.

---
+++ lz10_2/lz10_2.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+#include <cmath>
+#include <windows.h>
+using namespace std;
+
+int main() {
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+
+    double x;
+    cout << "Введіть значення x: ";
+    cin >> x;
+
+    double S = 0; // сума
+
+    for (int k = 1; k <= 12; k++) {
+        S += (sin(k * x) + k) / (pow(x + 0.1, 1.0 / k) + 6 * k);
+    }
+
+    cout << "Результат обчислення суми: " << S << endl;
+
+    return 0;
+}
